87d761573aa24d18276f56dc73c4dd7cdc09e875
wiley <weixiong.zhang@xatali.com>
2025-06-20 11:00:54 +0800

    fix：remove output

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // 启用静态导出
-  images: {
-    unoptimized: true, // 静态导出时需要禁用图片优化
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // 允许所有外部图片
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
